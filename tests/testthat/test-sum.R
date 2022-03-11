b2ce7f5e5f09efc936536442445b45e56e68cf45
Bai Li <bai.li@noaa.gov>
2022-03-11 14:13:40 -0500

    add testthat pkg name

--- tests/testthat/test-sum.R
+++ tests/testthat/test-sum.R
@@ -1,15 +1,15 @@
 test_that("sum works with a vector that has more than 1 number", {
     a <- c(1, 2)
-    expect_equal(ghBackup::sum(a), 3)
+    testthat::expect_equal(ghBackup::sum(a), 3)
 
     b <- c(1, 2, 5, 10)
-    expect_equal(ghBackup::sum(b), 18)
+    testthat::expect_equal(ghBackup::sum(b), 18)
 
     c <- c(2)
-    expect_error(ghBackup::sum(c), "vector x needs to have at least two numbers!")
+    testthat::expect_error(ghBackup::sum(c), "vector x needs to have at least two numbers!")
 })
 
 test_that("sum does not work with non numeric values", {
     a <- c("a", "b", "c")
-    expect_error(ghBackup::sum(a))
+    testthat::expect_error(ghBackup::sum(a))
 })
