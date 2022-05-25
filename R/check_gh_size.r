bb68c276ec21b5e6f657fd6a7ee3672ce90c4c4e
GiselleSchmitz-NOAA <101413268+GiselleSchmitz-NOAA@users.noreply.github.com>
2022-05-25 09:31:34 -0400

    Update check_gh_size.r
    Added documentation to function

--- R/check_gh_size.r
+++ R/check_gh_size.r
@@ -1,12 +1,25 @@
-#' Check size of a directory
-#' @export
+# Check size of a directory
+#
+# @description
+# The function uses identified path to check the size of given directory. It returns the total size of files in the directory.
+# @importFrom (specified path)
+#
+# @param path The full path to directory to be sized
+# @param full.names (needed? nested in function)
+#
+# @return Sum of files in directory
+# @examples
+# dir_size(
+#   path= "C:\Users\gizelle.schmitz\Desktop", recursive= TRUE) (?)
+#
+#
 # dir_size: main function to check size of a directory
 dir_size <- function(path, recursive = TRUE) {
 
   # error message class
   if (class(path) != "character") stop("class of the path is not character")
   files <- list.files(path, full.names = T, recursive = T)
-  
+
   if (length(files)==0) {
     size_files <- file.size(path)
 # Error message
