4198502083ff7c52cd4f0785dc283b6ddb1110b0
Bai Li <bai.li@noaa.gov>
2022-09-27 09:13:39 -0400

    test: update R test and testthat data

--- tests/testthat/test-back-up-gh.R
+++ tests/testthat/test-back-up-gh.R
@@ -18,18 +18,3 @@ test_that("back_up_gh_orgs() stops on error", {
     )
   )
 })
-
-test_that("back_up_gh_orgs() clones repos and creates zip archives", {
-  back_up_gh_orgs(
-    gh_token = gh::gh_token(),
-    backup_path = temp_dir,
-    orgs_name = c("nmfs-general-modeling-tools")
-  )
-
-  zip_path <- file.path(dirname(temp_dir), "ghBackup.zip")
-  on.exit(unlink(zip_path), add = TRUE)
-
-  testthat::expect_true(
-    file.exists(zip_path)
-  )
-})
